2cf2eacd9c9748c50152d22c173e9234c0a704e5
TunKitter <tunkitjava@gmail.com>
2025-08-20 16:23:58 +0700

    [feature]: create node with button or shortcut (n), fix conflict elements when change size, [remove]: panzoom, some messy stuff

--- script.js
+++ script.js
@@ -1,38 +1,77 @@
 const renderDirections = ["nw", "ne", "sw", "se"];
-const parent = document.querySelector(`.container`);
-const children = document.querySelectorAll(`.target`);
-let zoom = panzoom(parent, { maxZoom: 2, minZoom: 0.1 })
-children.forEach(child => {
-
-    child.onclick = function (e) {
-        e.stopPropagation()
-        zoom && zoom.pause()
-        const moveable = new Moveable(parent, {
-            target: child,
-            scalable: true,
-            draggable: true,
-            resizable: true,
-            renderDirections: renderDirections,
-            pinchable: true,
-            useResizeObserver: true,
-            origin: false,
-            edge: true,
-        });
-        moveable.on("resize", ({ target, width, height, dist, delta, clientX, clientY }) => {
-            console.log("onResize", target);
-            delta[0] && (target.style.width = `${width}px`);
-            delta[1] && (target.style.height = `${height}px`);
-            target.style.fontSize = `${Math.min(width, height) / 4}px`;
-        })
-
-        moveable.on("drag", e => {
-            e.target.style.transform = e.transform;
-        })
-        document.body.onclick = function () {
-            moveable.destroy()
-            zoom.resume()
-        }
-
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'n') {
+        createNode(document.querySelector('button'))
     }
+})
+function createNode(obj) {
+    obj.classList.add('btn-selected')
+    document.querySelector('.root').addEventListener('click', function (e) {
+        const element = document.createElement('div')
+        element.className = "target"
+        element.style.left = `${e.clientX}px`
+        element.style.top = `${e.clientY}px`
+        element.style.transform = `translate(-50%, -50%)`
+        element.innerHTML = "Target"
+        document.querySelector(".container").appendChild(element)
+        obj.classList.remove('btn-selected')
+    }, { once: true });
+};
+const selectoRef = new Selecto({
+    container: document.querySelector(`.select`),
+    dragContainer: document.querySelector(`.root`),
+    selectableTargets: [".target"],
+    hitRate: 0,
+    selectByClick: true,
+    selectFromInside: false,
+    toggleContinueSelect: ["shift"],
+    ratio: 0
+});
+const moveable = new Moveable(document.querySelector(`.container`), {
+    scalable: true,
+    draggable: true,
+    resizable: true,
+    renderDirections: renderDirections,
+    pinchable: true,
+    useResizeObserver: true,
+    origin: false,
+    edge: true,
+});
+moveable.on("resize", ({ target, width, height, dist, delta, clientX, clientY }) => {
+    // console.log("onResize", target);
+    delta[0] && (target.style.width = `${width}px`);
+    delta[1] && (target.style.height = `${height}px`);
+    target.style.fontSize = `${Math.min(width, height) / 4}px`;
+    // console.log(target);
 
 })
+
+
+moveable.on("drag", e => {
+    e.target.style.transform = e.transform;
+})
+
+moveable.on("clickGroup", e => {
+    selectoRef.clickTarget(e.inputEvent, e.inputTarget);
+});
+moveable.on("drag", e => {
+    e.target.style.transform = e.transform;
+});
+moveable.on("dragGroup", e => {
+    e.events.forEach(ev => {
+        ev.target.style.transform = ev.transform;
+    });
+});
+selectoRef.on("dragStart", e => {
+    const target = e.inputEvent.target;
+    if (moveable.isMoveableElement(target)) e.stop();
+});
+selectoRef.on("selectEnd", e => {
+    if (e.isDragStart) {
+        e.inputEvent.preventDefault();
+        moveable.waitToChangeTarget().then(() => {
+            moveable.dragStart(e.inputEvent);
+        });
+    }
+    moveable.target = e.selected;
+});
