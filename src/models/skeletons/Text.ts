305bc71dc6ce2043b61c3e6d41ee6f5df1bd1865
TunKitter <tunkitjava@gmail.com>
2025-09-15 22:04:47 +0700

    [feature]: selector property item, [remove]: duplicate range property item in index.html, .circle css, [selector]: font options for text skeleton

--- src/models/skeletons/Text.ts
+++ src/models/skeletons/Text.ts
@@ -1,13 +1,22 @@
 import Skeleton from "../enities/Skeleton";
 import '../../css/skeleton/text.css'
 import SkeletonProperty from "../enities/SkeletonProperty";
-import RangePropertyItem from "../enities/property_items/Range";
 import createSkeletonFromToolbarAndProperty from "../../flow/createSkeletonFromToolbar";
+import SelectorPropertyItem from "../enities/property_items/Selector";
 export default function createTextSkeletonAndPropertyFlow() {
     createSkeletonFromToolbarAndProperty(0, () => {
         const prop = new SkeletonProperty()
         const text_ = createTextSkeleton('Hello')
-        const item = new RangePropertyItem('Font size', text_.getELement())
+        const item = new SelectorPropertyItem('Font', text_.getELement())
+        item.addOption('Times New Roman', 'times-new-roman')
+        item.addOption('Arial', 'arial')
+        item.addOption('Courier New', 'courier-new')
+        item.addOption('Georgia', 'georgia')
+        item.addOption('Verdana', 'verdana')
+        item.handleChange((value, referenceElement) => {
+            // @ts-ignore
+            referenceElement.style.fontFamily = value;
+        })
         prop.addItem(item)
         text_.doubleClickToEdit()
         return [text_, prop]
