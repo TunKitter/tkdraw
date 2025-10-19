83a32d630f9cb4288e19b931f6173a2c1c76cf3d
TunKitter <tunkitjava@gmail.com>
2025-10-19 22:22:30 +0700

    [feature]: show zoom info

--- src/lib/infinite_viewer.ts
+++ src/lib/infinite_viewer.ts
@@ -5,6 +5,16 @@ export function initInfiniteView() {
   G.infinite_viewer = new InfiniteViewer(G.app_element, {
     displayHorizontalScroll: false,
     displayVerticalScroll: false,
+    zoomRange: [0.01, 3],
     useAutoZoom: true
+  });
+  const info = document.querySelector('#zoom p')!;
+  G.infinite_viewer.on('scroll', e => {
+    info.textContent = Math.round(e.zoomX * 100) + '%';
+  });
+  document.querySelector('#zoom')!.addEventListener('dblclick', e => {
+    e.preventDefault();
+    G.infinite_viewer.setZoom(1);
+    info.textContent = '100%';
   });
 }
